713e1da4dc95ebc2e849a57b1a2f8c6286f70c46
Tyler Dinh <linh.dinh@orientsoftware.net>
2018-10-26 14:41:54 +0700

    Entity Class

---
+++ BusinessLogic.Test/CustomerTest.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using BusinessLogic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BusinessLogic.Test
+{
+    [TestClass]
+    public class CustomerTest
+    {
+        [TestMethod]
+        public void FullNameTestValid()
+        {
+            // Arrange
+            Customer customer = new Customer();
+            customer.FirstName = "Linh";
+            customer.LastName = "Duy";
+            string expected = "Duy, Linh";
+
+            // Act
+            string actual = customer.FullName;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameFirstNameEmpty()
+        {
+            // Arrange
+            Customer customer = new Customer();
+            customer.LastName = "Duy";
+            string expected = "Duy";
+
+            // Act
+            string actual = customer.FullName;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void FullNameLastNameEmpty()
+        {
+            // Arrange
+            Customer customer = new Customer();
+            customer.FirstName = "Linh";
+            string expected = "Linh";
+
+            // Act
+            string actual = customer.FullName;
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void ObjectReferenceTest()
+        {
+            //Arrange
+            var c1 = new Customer();
+            c1.FirstName = "Linh";
+            var c2 = c1;
+            c2.FirstName = "New Linh";
+            string expected = "New Linh";
+
+            //Act
+
+            //Assert
+            Assert.AreEqual(c1.FirstName, expected);
+
+        }
+
+        [TestMethod]
+        public void StaticTest()
+        {
+            //Arrange
+            var c1 = new Customer();
+            Customer.InstanceCount += 1;
+            var c2 = new Customer();
+            Customer.InstanceCount += 1;
+            var c3 = new Customer();
+            Customer.InstanceCount += 1;
+
+            //Act
+
+            //Assert
+            Assert.AreEqual(Customer.InstanceCount, 3);
+        }
+    }
+}
---
+++ BusinessLogic/Customer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class Customer
+    {
+        public static int InstanceCount { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string FullName
+        {
+            get
+            {
+                string fullName = LastName;
+                if(!string.IsNullOrWhiteSpace(FirstName))
+                {
+                    if(!string.IsNullOrWhiteSpace(fullName))
+                    {
+                        fullName += ", ";
+                    }
+                    fullName += FirstName;
+                }
+                return fullName;
+            }
+        }
+
+        public string EmailAddress { get; set; }
+
+        public int CustomerId { get; private set; }
+
+    }
+}
